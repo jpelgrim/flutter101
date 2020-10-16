091331325cb1b0ff3a476d78fe84168e82d61f88
Johan Pelgrim <johan.pelgrim@gmail.com>
2020-10-17 01:01:10 +0200

    Add GestureDetector with onTap which starts audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter101/audio.dart';
 
 void main() {
   runApp(Flutter101App());
@@ -8,6 +8,7 @@ void main() {
 class Flutter101App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final audio = Audio('trumpet.mp3');
     return MaterialApp(
         title: 'Flutter 101',
         theme: ThemeData(
@@ -18,12 +19,15 @@ class Flutter101App extends StatelessWidget {
             title: Text('Flutter 101'),
           ),
           body: Center(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12.0),
-              child: Image.asset(
-                'assets/images/trumpet.jpg',
-                width: 100,
-                height: 100,
+            child: GestureDetector(
+              onTap: () => audio.start(),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12.0),
+                child: Image.asset(
+                  'assets/images/trumpet.jpg',
+                  width: 100,
+                  height: 100,
+                ),
               ),
             ),
           ),
